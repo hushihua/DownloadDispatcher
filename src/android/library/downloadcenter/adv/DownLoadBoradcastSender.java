6b7e17aadf25a64d61c07974006e94af73b781bc
pritywawa <pritywawa@163.com>
2016-03-24 10:36:46 +0800

    asdf

--- src/android/library/downloadcenter/adv/DownLoadBoradcastSender.java
+++ src/android/library/downloadcenter/adv/DownLoadBoradcastSender.java
@@ -26,7 +26,7 @@ import android.content.Intent;
  */
 public class DownLoadBoradcastSender {
 
-	public static String REFRESH_DOWNLOAD_LIST_ACTION = "android.library.framework.download:REFRESH_CACHE_LIST";
+	public static String REFRESH_DOWNLOAD_LIST_ACTION = "android.library.framework.download:REFRESH_CACHE_LIST_asdfdsfsd";
 	public static String DOWNLOAD_ITEM_FINISH_ACTION = "android.library.framework.download:ITEM_FINISH";
 
 	public static void sendRefreshDownloadCacheBroadcast(Context context) {
